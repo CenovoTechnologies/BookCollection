8dbcae88804b23a0570cad83b7a307f0fd570cb5
Melissa S Smith <Melissa@cenovotechnologies.com>
2017-11-12 17:42:13 -0800

    Get a basic UI started

--- BookCollection/AddEditBookPage.xaml.cs
+++ BookCollection/AddEditBookPage.xaml.cs
@@ -3,10 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
-using System.Windows;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -15,22 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace BookCollection
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class AddEditBookPage : Page
     {
-        public MainPage()
+        public AddEditBookPage()
         {
             this.InitializeComponent();
-        }
-
-        private async void OnClick_AddBookBtn(object sender, RoutedEventArgs e) {
-            this.Frame.Navigate(typeof(AddEditBookPage));
         }
     }
 }
