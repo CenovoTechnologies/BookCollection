93ebe174b7ab4a30ffc1410bc723d35e0ea2fc55
Melissa S Smith <Melissa@cenovotechnologies.com>
2017-12-23 22:16:54 -0800

    Front-end workflow and movement

--- bookcollection/index.js
+++ bookcollection/index.js
@@ -6,8 +6,10 @@ const {app, BrowserWindow, Menu, ipcMain} = electron;
 
 let mainWindow;
 let addCollectionWindow;
+let addUserWindow;
+let loginWindow;
+let addBookWindow;
 
-//Listen for the app to be ready
 app.on('ready', function() {
 	mainWindow = new BrowserWindow({});
 
@@ -24,7 +26,7 @@ app.on('ready', function() {
 	Menu.setApplicationMenu(mainMenu);
 });
 
-function createAddWindow() {
+function createCollectionWindow() {
 	addCollectionWindow = new BrowserWindow({
 		width: 300,
 		height: 200,
@@ -32,7 +34,7 @@ function createAddWindow() {
 	});
 
 	addCollectionWindow.loadURL(url.format({
-		pathname: path.join(__dirname, 'addCollectionWindow.html'),
+		pathname: path.join(__dirname, 'createCollection.html'),
 		protocol: 'file:',
 		slashes: true
 	}));
@@ -42,9 +44,113 @@ function createAddWindow() {
 	});
 }
 
+function createAddUserWindow() {
+	addUserWindow = new BrowserWindow({
+		width: 500,
+		height: 500,
+		title:'Create a New User Account'
+	});
+
+	addUserWindow.loadURL(url.format({
+		pathname: path.join(__dirname, 'createUserAccount.html'),
+		protocol: 'file:',
+		slashes: true
+	}));
+
+	addUserWindow.on('close', function() {
+		addUserWindow = null;
+	});
+}
+
+function createLoginWindow() {
+	loginWindow = new BrowserWindow({
+		width: 300,
+		height: 200,
+		title: 'Login'
+	});
+
+	loginWindow.loadURL(url.format({
+		pathname: path.join(__dirname, 'login.html'),
+		protocol: 'file:',
+		slashes: true
+	}));
+
+	loginWindow.on('close', function() {
+		loginWindow = null;
+	});
+}
+
+function createAddBookWindow() {
+	addBookWindow = new BrowserWindow({
+		width: 500,
+		height: 500,
+		title: 'Add New Book'
+	});
+
+	addBookWindow.loadURL(url.format({
+		pathname: path.join(__dirname, 'editBook.html'),
+		protocol: 'file:',
+		slashes: true
+	}));
+
+	addBookWindow.on('close', function() {
+		addBookWindow = null;
+	});
+}
+
+ipcMain.on('book:add', function(e, title, author) {
+	mainWindow.webContents.send('book:add', title, author);
+	addBookWindow.close();
+})
+
 ipcMain.on('collection:add', function(e, item) {
 	mainWindow.webContents.send('collection:add', item);
 	addCollectionWindow.close();
+});
+
+ipcMain.on('collection:open', function(e, collectionId) {
+	mainWindow.loadURL(url.format({
+		pathname: path.join(__dirname, 'viewCollection.html'),
+		protocol: 'file:',
+		slashes: true
+	}));
+	mainWindow.webContents.on('did-finish-load', function() {
+		mainWindow.webContents.send('collection:open', collectionId);
+	});
+});
+
+ipcMain.on('userAccount:add', function(e, item) {
+	mainWindow.loadURL(url.format({
+		pathname: path.join(__dirname, 'homepage.html'),
+		protocol: 'file:',
+		slashes: true
+	}));
+	addUserWindow.close();
+});
+
+ipcMain.on('userAccount:login', function(e, email, password) {
+	mainWindow.loadURL(url.format({
+		pathname: path.join(__dirname, 'homepage.html'),
+		protocol: 'file:',
+		slashes: true
+	}));
+	loginWindow.close();
+});
+
+ipcMain.on('loginBtn:click', function(e) {
+	createLoginWindow();
+});
+
+ipcMain.on('createAccountBtn:click', function(e) {
+	createAddUserWindow();
+});
+
+ipcMain.on('addCollectionBtn:click', function(e) {
+	createCollectionWindow();
+});
+
+ipcMain.on('addBookBtn:click', function(e) {
+	createAddBookWindow();
 });
 
 const mainMenuTemplate = [
@@ -52,13 +158,16 @@ const mainMenuTemplate = [
 		label:'File',
 		submenu:[
 			{
-				label: 'Add New Collection',
+				label: 'Create a New Account',
 				click(){
-					createAddWindow();
+					createAddUserWindow();
 				}
 			},
 			{
-				label: 'Open Collection'
+				label: 'Login',
+				click(){
+					createLoginWindow();
+				}
 			},
 			{
 				label: 'Quit',
@@ -66,20 +175,6 @@ const mainMenuTemplate = [
 				click(){
 					app.quit();
 				}
-			}
-		]
-	},
-	{
-		label:'Edit',
-		submenu:[
-			{
-				label:'Add New Book'
-			},
-			{
-				label:'Edit Book',
-			},
-			{
-				label:'Remove Book(s)'
 			}
 		]
 	}
